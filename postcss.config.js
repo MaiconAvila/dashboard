5cf7687c4cd270b80668690f160193e0debe931c
MaiconAvila <maicon.avila@impar.com.br>
2022-12-08 08:07:06 -0300

    reinstall tailwind and dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-  plugins: [
-    require("tailwindcss")("./src/tailwind.config.js"),
-    {
-      'postcss-import': {},
-      'tailwindcss/nesting': {},
-      tailwindcss: {},
-      autoprefixer: {},
-    }
-  ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx,html.js}',
-    './node_modules/tw-elements/dist/js/**/*.js'
+    "./src/**/*.{js,jsx,ts,tsx,html.js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   plugins: [
-    require('tw-elements/dist/plugin')
+    require("tw-elements/dist/plugin")
   ]
 }
